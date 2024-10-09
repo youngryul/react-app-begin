a9ad650a2588c8c3c2f7ab7049b5b1c51e5f81b1
Leeyoungryul <lekjkj@naver.com>
2024-10-09 23:50:21 +0900

    10/9 useEffect 이론 학습

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Button from "./Button";
 import styles from "./App.module.css"
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-        <h1 className={styles.title}>Welcome back!</h1>
-        <Button text={"Continue"}/>
-    </div>
-  );
+    const [counter, setValue] = useState(0);
+    const onClick = () => setValue((prev) => prev + 1);
+    console.log("call an api");
+    return (
+        <div>
+            <h1 className={styles.title}>{counter}</h1>
+            <button onClick={onClick}>click me</button>
+        </div>
+    );
+
 }
 
 
